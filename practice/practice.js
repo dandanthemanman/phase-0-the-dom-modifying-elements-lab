19dcbb6af90a1eb433c9e9afa333b8b754516c49
Daniel Becker <daniel.becker000@gmail.com>
2022-03-24 09:43:14 -0700

    commit tree 120ed170836611774acf7cf9dbee4ef3160f1c08

---
+++ practice/practice.js
@@ -0,0 +1,17 @@
+// let instrumentList = document.getElementById("instruments")
+// let childNodes = instrumentList.childNodes 
+// console.log(childNodes) 
+
+const listItems = document.getElementsByTagName('li')
+
+
+// const listItems= document.querySelectorAll ('li')
+// console.log(listItems)
+
+// for (let i=0; i <listItems.length; i++) {
+//     listItems[i].style.color = "orange"
+// }
+console.log(listItems)
+listItems[3].style.color = "green"
+
+// listItems[1].style.color = "red"
